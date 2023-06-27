0929913eaa433810e2b88f4a869f87c2c5c0858a
Fabio <aringolifabio@gmail.com>
2023-06-27 18:00:19 -0300

    data

---
+++ models/Manga.js
@@ -0,0 +1,24 @@
+import { Schema,model,Types } from "mongoose";
+
+let collection = "mangas"
+let schema = new Schema({
+    author_id:{
+        type:Types.ObjectId,
+        ref:"users",
+        required:true
+    },
+    title: {type:String,required:true},
+    cover_photo:{type:String,required:true},
+    description:{type:String,required:true},
+    category_id:{
+        type:Types.ObjectId,
+        ref:"users",
+        required:true
+    },
+},{
+    timestamps:true
+})
+
+const Manga = model(collection,schema)
+
+export default Manga
